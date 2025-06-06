c6fcaa615e4c5393752ef7169f68453da147908c
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-06 10:35:04 +0530

    first version

---
+++ day1/SumOfDigits.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class SumOfDigits {   
+	    public static int sumDigits(int n) {
+	        if (n == 0) {
+	            return 0;
+	        }
+	        return (n % 10) + sumDigits(n / 10);  
+	    }
+	    public static void main(String[] args) {
+	        int number = 45678;
+	        int result = sumDigits(number);
+	        System.out.println("Sum of digits of " + number + " is: " + result);
+	    }
+	}
+
+
